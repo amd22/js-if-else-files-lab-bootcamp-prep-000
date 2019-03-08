3b9b4ef5f162af45435f86ba660d5c6f2f973ae6
Ahmed Azam <amdnew1@gmail.com>
2019-03-08 17:36:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var height = "74"
 module.exports = { name, height
 }
 
+
+
+if (name === "susan"){
+   console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
+
+var name = "Susan"
